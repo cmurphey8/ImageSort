f0e91d8f4597edbf5ab84b83d6ae7b71e8caf887
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2021-05-06 13:37:48 -0400

    Update helpers.h

--- helpers.h
+++ helpers.h
@@ -6,8 +6,5 @@ void transform(int height, int width, RGBTRIPLE image[height][width]);
 // selection sort by hue
 void selectionSort(int height, int width, RGBTRIPLE image[height][width]);
 
-// insertion sort by hue
-void insertionSort(int height, int width, RGBTRIPLE image[height][width]);
-
 // try combinations of sorting methods
 void combinationSort(int height, int width, RGBTRIPLE image[height][width]);
